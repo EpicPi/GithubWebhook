ddbc3ebb8608dcb182eecf581dd6c3bc1fb3ec03
EpicPi <piyushgk1@gmail.com>
2020-04-11 23:35:30 -0400

    git pull params were wrong

--- githubupdate.sh
+++ githubupdate.sh
@@ -4,6 +4,6 @@ eval `/usr/bin/ssh-agent` > /dev/null
 ssh-add ~/.ssh/id_rsa
 
 cd /var/www/base
-git pull origin/master
+git pull origin master
 cd /var/www/gyms
-git pull origin/gym-landing-page
+git pull origin gym-landing-page
